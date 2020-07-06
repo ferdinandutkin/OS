8a90a130e95178abbaf377c59db1ac2670fb4f35
ferdinandutkin <61806555+ferdinandutkin@users.noreply.github.com>
2020-07-06 23:37:16 +0300

    Add files via upload

---
+++ Таймеры/WindowsProject1/Resource.h
@@ -0,0 +1,33 @@
+﻿//{{NO_DEPENDENCIES}}
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется WindowsProject1.rc
+
+#define IDS_APP_TITLE			103
+#define TIMERX				    78
+#define TIMERY                  79
+#define TIMERS                  80
+#define IDR_MAINFRAME			128
+#define IDC_TIMER_PROC			109
+#define IDD_WINDOWSPROJECT1_DIALOG	102
+#define IDD_ABOUTBOX			103
+#define IDM_ABOUT				104
+#define IDM_EXIT				105
+#define IDI_WINDOWSPROJECT1			107
+#define IDI_SMALL				108
+#define IDC_WINDOWSPROJECT1			109
+#define IDC_MYICON				2
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+// Следующие стандартные значения для новых объектов
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
